e1135b9c22973e407324b129f0e4fa41548482d0
hoanggtin <vuhoangtin@gmail.com>
2023-10-24 15:30:03 +0700

    Demo Interface
    update 24/10

---
+++ Interface/test1.py
@@ -0,0 +1,64 @@
+import test
+import time
+import threading
+import cv2
+
+#   HÀM CHẠY REAL-TIME NHIỆT ĐỘ ĐỘ ẨM
+def setT():                          
+    t=10
+    o=50
+    while True:
+        gui.setTemp(t, o)
+        time.sleep(5)
+        t+=10
+        o+=1
+# HÀM CHẠY REAL TIME PHÁT VIDEO
+def set_frame():            
+    # time.sleep(5)          
+    ret, frame = cap.read()
+    while ret:
+        ret, frame = cap.read()
+        gui.setCapture(frame)
+        time.sleep(1/24)
+
+#   HIỂN THỊ MÀN HÌNH
+gui = test.GUI() 
+gui.draw()
+gui.setSize()
+
+#HIỂN THỊ NHIỆT ĐỘ ĐỘ ẨM - REAL TIME
+gui.setTemp('22','56%')
+gui.setMonitor()
+gui.setTimer_Monitor()
+t = threading.Thread(target=setT, args=[])
+t.setDaemon(True)
+t.start()
+
+#HIỂN THỊ VIDEO
+cap = cv2.VideoCapture('video.mp4')
+t2 = threading.Thread(target=set_frame, args=())
+t2.setDaemon(True)
+t2.start()
+gui.setTimer()
+
+# HIỂN THỊ HISTORY
+history = ["Tín","23-12-9 8:30", "23-12-9 9:30", "Hiếu", "23-12-9 8:30", "23-12-9 9:30", "Phúc", "23-12-9 8:30", "23-12-9 9:30"]
+gui.setH(history)
+gui.setHistory()
+gui.setTimer_History()
+
+# HIỂN THỊ LIST
+list = ["Tín", "21TCLC_KHDL2", "CNTT", "Hiếu", "21TCLC_KHDL2", "CNTT", "Phúc", "21TCLC_KHDL2", "CNTT"]
+gui.setL(list)
+gui.setList()
+gui.setTimer_List()
+
+
+#HIỂN THỊ BIỂU ĐỔ
+gui.setTemperature()
+gui.setTimer_Temperature()
+
+gui.exitWin()
+
+
+
